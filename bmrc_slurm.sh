74b12d4c7afcbec6e9b19c33d1fb960c5c88f0ee
psamonwong <phrutsamon.wongnak@gmail.com>
2023-11-28 11:14:25 +0700

    qmd updates

--- bmrc_slurm.sh
+++ bmrc_slurm.sh
@@ -7,7 +7,7 @@
 #SBATCH -o /well/moru-batty/users/lcd199/PLATCOV-SAP/PLATCOV-Fluoxetine/o_and_e_files/output.o%A_%a.out
 #SBATCH -e /well/moru-batty/users/lcd199/PLATCOV-SAP/PLATCOV-Fluoxetine/o_and_e_files/output.e%A_%a.out
 #SBATCH -p short
-#SBATCH --array 1-8
+#SBATCH --array 1-32
 
 
 echo started=`date`
@@ -20,6 +20,6 @@ echo "job=$SLURM_JOB_ID"
 echo "hostname="`hostname`
 echo "OS="`uname -s`
 echo "username="`whoami`
-Rscript /well/moru-batty/users/lcd199/PLATCOV-SAP/PLATCOV-Fluoxetine/run_models_new.R ${SLURM_ARRAY_TASK_ID} --no-save --no-restore
+Rscript /well/moru-batty/users/lcd199/PLATCOV-SAP/PLATCOV-Fluoxetine/run_models_cluster.R ${SLURM_ARRAY_TASK_ID} --no-save --no-restore
 echo "finished="`date`
 exit 0
